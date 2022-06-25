e4a0a7730ef37d7951eedb9b855a16325498aa21
Gathin23 <gathint23@gmail.com>
2022-06-25 22:43:09 +0530

    Initial setup for config and  app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from "react";
+import {ethers} from "ethers"
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json"
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 function App() {
+  const [greeting, setGreetingValue] = useState("")
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
